99ab07eee0f622807ae8aac8d855b884481e7d43
Steven Thompson <steven.thompson.ca@gmail.com>
2016-03-22 11:41:58 -0400

    Initial Commit

---
+++ ImagePicker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImagePicker
+//
+//  Created by steve on 2016-03-22.
+//  Copyright © 2016 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImagePicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagePicker
+//
+//  Created by steve on 2016-03-22.
+//  Copyright © 2016 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
