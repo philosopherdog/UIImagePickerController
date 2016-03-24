b043b5e088cc854898a931e129abc1d5cf682656
Steven Thompson <steven.thompson.ca@gmail.com>
2016-03-24 16:20:29 -0400

    Adds video player, etc.

--- ImagePicker/ViewController.m
+++ ImagePicker/ViewController.m
@@ -8,38 +8,115 @@
 
 #import "ViewController.h"
 
+// for AVPlayer
+@import AVFoundation;
+
+//  for AVPlayerViewController
+@import AVKit;
+
 @interface ViewController ()<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
+#pragma mark - Button Actions
+
+- (IBAction)albumTapped:(UIBarButtonItem *)sender {
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeSavedPhotosAlbum]) {
+        return;
+    }
+    UIImagePickerController *ipc = [[UIImagePickerController alloc] init];
+    ipc.delegate = self;
+    ipc.sourceType = UIImagePickerControllerSourceTypeSavedPhotosAlbum;
+    ipc.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:
+                      UIImagePickerControllerSourceTypeCamera];
+    [self presentViewController:ipc animated:YES completion:^{
+        NSLog(@"%s", __PRETTY_FUNCTION__);
+    }];
+}
+
+- (IBAction)libraryTapped:(UIBarButtonItem *)sender {
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
+        return;
+    }
+    UIImagePickerController *ipc = [[UIImagePickerController alloc] init];
+    ipc.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    ipc.delegate = self;
+    ipc.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:
+                      UIImagePickerControllerSourceTypeCamera];
+    [self presentViewController:ipc animated:YES completion:^{
+        NSLog(@"%s", __PRETTY_FUNCTION__);
+    }];
 }
 
 - (IBAction)cameraTapped:(UIBarButtonItem *)sender {
+    
+    if (![UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        // make a UIAlertController
+        return;
+    }
+    
     UIImagePickerController *ipc = [[UIImagePickerController alloc] init];
     ipc.delegate = self;
     NSArray *sourceTypes = [UIImagePickerController availableMediaTypesForSourceType:UIImagePickerControllerSourceTypeCamera];
     NSLog(@"%@", sourceTypes);
-    
-    BOOL isCamera = sender.tag == 1 ? YES : NO;
-    
-    if (isCamera && [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
-        ipc.sourceType = UIImagePickerControllerSourceTypeCamera;
-    } else if (!isCamera && [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
-        ipc.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-    }
+    ipc.sourceType = UIImagePickerControllerSourceTypeCamera;
+    ipc.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType:
+      UIImagePickerControllerSourceTypeCamera];
     [self presentViewController:ipc animated:YES completion:nil];
 }
 
+#pragma mark - Delegate Methods
+
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    NSLog(@"%@", info);
     UIImage *image = info[UIImagePickerControllerOriginalImage];
     self.imageView.image = image;
-    [self dismissViewControllerAnimated:picker completion:nil];
+    [self dismissViewControllerAnimated:YES completion:^ {
+        if ([info[UIImagePickerControllerMediaType] isEqualToString:@"public.movie"]) {
+            NSLog(@"is movie");
+            NSURL *url = info[UIImagePickerControllerMediaURL];
+            // play video
+            [self playVideoAtPath:url];
+            // [self saveMovieWithInfo:info];
+        }
+    }];
+}
+
+// play video
+- (void)playVideoAtPath:(NSURL *)path {
+    AVPlayer *player = [AVPlayer playerWithURL:path];
+    AVPlayerViewController *pvc = [AVPlayerViewController new];
+    pvc.player = player;
+    //    [pvc.player play]; // if you want to start by playing
+    [self presentViewController:pvc animated:YES completion:nil];
+}
+
+#pragma mark - Saving Video
+
+// saves video to Photo Album
+- (void)saveMovieWithInfo:(NSDictionary<NSString *,id> *)info {
     
+    NSURL *url = info[UIImagePickerControllerMediaURL];
+    
+    if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum([url path])) {
+        // save it
+        UISaveVideoAtPathToSavedPhotosAlbum([url path], self, @selector(video:didFinishSavingWithError:contextInfo:), nil);
+    }
+}
+
+// completion handler for saving a video
+- (void)video:(NSString *)videoPath didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo{
+    
+    NSLog(@"Finished saving with error: %@", error.localizedDescription);
+    
+    // remove the temp file
+    NSError *err = nil;
+    [[NSFileManager defaultManager] removeItemAtPath:videoPath error:&err];
+    if (error) {
+        NSLog(@"%@", error.localizedDescription);
+    }
 }
 
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
